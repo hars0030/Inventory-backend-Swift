cc8455c5febd67c6909cd7b82107f9f75879da4c
HARSH KUMAR <harshkumar@HARSHs-MacBook-Pro.local>
2025-02-12 10:05:39 -0500

    working on database for CRUD operations on part database

--- Sources/Inventory/Database.swift
+++ Sources/Inventory/Database.swift
@@ -5,9 +5,51 @@
 //  Created by HARSH KUMAR on 2025-02-11.
 //
 
-struct Database {
-    let parts: [Part] = []
-    let warehouses: [Warehouse] = []
+import Foundation
+
+
+actor Database {
+    var parts: [Part] = []
+    var warehouses: [Warehouse] = []
+    
+    
+    static let shared = Database()
+
+    
+    private init() {}
+    
+
+    
+    func addPart(_ part:Part) {
+        parts.append(part)
+         
+    }
+    
+    
+    
+    func getPart (by uuid :UUID) throws -> Part? {
+       return parts.first(where: {$0.id == uuid})
+        }
+    
+    func getAllParts () -> [Part] {
+        
+        return parts
+    }
+    
+//    func updatePart (_ updatedPart:Part) throws -> Part? {
+//        if let index = parts.firstIndex(of: {$0.id == updatedPart.id })
+//    }
+//        
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 }
+
+
--- Sources/Inventory/Repository/PartRepository.swift
+++ Sources/Inventory/Repository/PartRepository.swift
@@ -23,3 +23,44 @@ protocol PartRepository: Sendable {
 }
 
 
+
+class PartRepositoryImpl : PartRepository {
+    
+    private let database : Database
+    
+    init(database: Database) {
+        self.database = database
+    }
+    
+    func create(name: String, category: PartCategory, size: Dimensions?, weight: Double?) async throws -> Part? {
+         let partWeight = weight ?? 0
+        
+        let part  = Part(id: UUID(), name: name, category: category, size: size, weight: partWeight)
+        
+        await  Database.shared.addPart(part)
+        
+        
+            
+        return part
+    }
+    
+    func get(id: UUID) async throws -> Part? {
+        try await Database.shared.getPart(by: id)
+    }
+    
+    func list() async throws -> [Part] {
+        await Database.shared.getAllParts()
+        
+        
+    }
+    
+    func update(id: UUID, name: String, category: PartCategory, size: Dimensions?, weight: Double?) async throws -> Part? {
+        
+        let updatedPart = Part(id: id, name: name, category: category, size: size, weight: weight)
+        
+        await Database.shared.updatePart()
+    }
+    
+    
+    
+}
